8d95155e0c55ca553a73d0a1e676e06dcb4fbbd7
heraskoa <ander1998g@gmail.com>
2019-10-28 00:55:35 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,42 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__, static_url_path='', static_folder='static')
+
+
+def create_app(config_class=Config):
+    # ...
+    if not app.debug and not app.testing:
+        # ...
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/lab2.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('lab2 startup')
+
+    return app
+
+
+create_app(Config)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -5,3 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = "postgres://postgres@localhost:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
