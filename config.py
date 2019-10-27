d5b3160ba3f100596576c8c6be25b30620f760ce
heraskoa <ander1998g@gmail.com>
2019-10-28 00:59:52 +0200

    update

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres@localhost:5432/postgres"
+    #SQLALCHEMY_DATABASE_URI = "postgres://postgres@localhost:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
